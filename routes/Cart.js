57e2d7105b609aeb313439c590c1dcf36fe71ae2
rezaprasetya241 <rezaprasetya241@gmail.com>
2022-07-21 14:27:25 +0700

    update cart

--- routes/Cart.js
+++ routes/Cart.js
@@ -3,17 +3,34 @@ const router = express.Router()
 const {Cart, Products} = require('../models')
 const {validateToken} = require("../middlewares/AuthMiddleware")
 
-router.get("/carts/:id", async(req, res) => {
-    const id = req.params.id
-    // const UserId = req.user.id
-    const cart = await Products.findOne({
+router.post("/:id", validateToken, async(req, res) => {
+    const UserId = req.user.id
+    const Found = await Products.findOne({
         where: {
-            id: id
+            id: req.params.id
         }
     })
-    res.json(cart)
+    if(!Found){
+        return res.status(404).json({msg: "No Product found"})
+    } else{
+        // const cart = Found
+        // cart.UserId = UserId
+        // cart.ProductId = ProductId
+        await Cart.create({
+            titleParfum: Found.title,
+            urlParfum: Found.urlPic,
+            sumParfum: 1,
+            priceParfum: Found.priceMl,
+            ProductId: Found.id,
+            UserId: UserId,
+        })
+        res.status(200).json({msg: "Add Product Successfull"})
+    }
+    // res.json(Found.urlPic)
     // con
 })
-
+router.get("/", async(req, res) => {
+    
+})
 
 module.exports = router
